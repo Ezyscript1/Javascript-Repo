aecfd40470304fd1ffa32a4cfe7b626039c9b565
Ezyscript1 <adegokeisreal48@gmail.com>
2023-04-24 21:33:10 +0100

    Done with my modal project

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -6,20 +6,33 @@ const showModal = document.querySelectorAll('.show-modal');
 const closeModal = document.querySelector('.close-modal');
 // console.log(showModal);
 
+const hideModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
 for (let i = 0; i < showModal.length; i++) {
   //   console.log(showModal[i].textContent);
-  showModal[i].addEventListener('click', function () {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+  showModal[i].addEventListener('click', openModal);
 }
 
-closeModal.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-});
+closeModal.addEventListener('click', hideModal);
 
-overlay.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+overlay.addEventListener(
+  'click',
+  hideModal
+  // hideModal();
+);
+
+// keypress events
+document.addEventListener('keydown', function (e) {
+  // console.log('A key was pressed');
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    hideModal();
+  }
 });
