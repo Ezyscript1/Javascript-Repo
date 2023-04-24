28e8705a2817a3b4bfd649583c69f7ccd7286f17
Ezyscript1 <adegokeisreal48@gmail.com>
2023-04-25 00:40:30 +0100

    git commit -m "Done for now"

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,31 @@
 'use strict';
+
+// selcting elements
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnRoll = document.querySelector('.btn--roll');
+const btnNew = document.querySelector('.btn--new');
+const btnHold = document.querySelector('.btn--hold');
+const current0 = document.getElementById('current--0');
+const current = document.getElementById('current--1');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+let currentScore = 0;
+let activePlayer = 0;
+
+btnRoll.addEventListener('click', function () {
+  const diceNum = Math.trunc(Math.random() * 6) + 1;
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${diceNum}.png`;
+
+  if (diceNum !== 1) {
+    // add dice number to current score
+    currentScore += diceNum;
+    current0.textContent = currentScore;
+  } else {
+  }
+});
